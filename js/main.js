91f7fd32dbddff19642d07a0390cbb275ba96f80
sarah-ga <sarah.hallacher@generalassemb.ly>
2016-08-12 16:05:29 -0400

    unit-7-lesson-4

---
+++ js/main.js
@@ -0,0 +1,24 @@
+// Step 1: When the user clicks on the h1
+	// a) Fade toggle the img (which is hidden with css by default)
+
+
+// Step 2: When the user focuses on the input (the focus event)
+	// a) Add the active class to the input (which has been styled in the style.css)
+
+
+
+// Step 3: When the user clicks or tabs away from the input (the blur event)
+	// a) Remove the active class from the input
+
+
+
+// Step 4: Hide the item with the id #menu
+
+
+// Step 5: When the element with the id #dropdownButton is clicked,
+	// a) slide toggle #menu (look up the slideToggle method if you need a refresher)
+
+
+
+
+
