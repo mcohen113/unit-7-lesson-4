e1789978d3b127fd457b38197aa362a0731ced27
mike cohen <mcohen113@msn.com>
2017-05-23 17:07:19 +0800

    mouseoutworks

--- js/main.js
+++ js/main.js
@@ -1,18 +1,19 @@
 // Step 1: When the user clicks on the h1
-  $('h1').click('slow', function() {
-    $('img').fadeToggle();
-  })// a) Fade toggle the img (which is hidden with css by default)
+$('h1').click('slow', function() {
+  $('img').fadeToggle();
+})// a) Fade toggle the img (which is hidden with css by default)
 
 
 // Step 2: When the user focuses on the input (the focus event)
-	$('input').focus(function(event) {
-    $('input').addClass('active');/* Act on the event */
-  });// a) Add the active class to the input (which has been styled in the style.css)
-
+$('input').focus(function(event) {
+  $(this).addClass('active');/* Act on the event */
+});// a) Add the active class to the input (which has been styled in the style.css)
 
 
 // Step 3: When the user clicks or tabs away from the input (the blur event)
-	// a) Remove the active class from the input
+$('input').mouseout(function(event) {
+  $(this).removeClass('active')/* Act on the event */
+});	// a) Remove the active class from the input
 
 
 
